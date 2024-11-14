63bb4451cec622e5abc1a0f2d8e3338336b596e3
SightStudio <sdm3285@gmail.com>
2024-11-14 19:21:38 +0900

    refactor. 리뷰반영. TableAlias.java ThreadLocal로 전환

--- src/main/java/orm/TableAlias.java
+++ src/main/java/orm/TableAlias.java
@@ -1,14 +1,25 @@
 package orm;
 
-import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
 
 public record TableAlias(
         String tableName,
         String alias
 ) {
 
-    // table alias를 지정하지 않으면 랜덤으로 만든다.
+    private static final ThreadLocal<Map<String, Integer>> threadLocalTableNameCounter = ThreadLocal.withInitial(HashMap::new);
+
     public TableAlias(String tableName) {
-        this(tableName, "%s_%s".formatted(tableName, new Random().nextInt(1000)));
+        this(tableName, "%s_%s".formatted(tableName, generateTableAliasIndex(tableName)));
+    }
+
+    /**
+     * 스레드 기준으로 테이블 별 alias index 생성
+     */
+    private static Integer generateTableAliasIndex(String tableName) {
+        Map<String, Integer> counterMap = threadLocalTableNameCounter.get();
+        counterMap.put(tableName, counterMap.getOrDefault(tableName, 0) + 1);
+        return counterMap.get(tableName);
     }
 }
